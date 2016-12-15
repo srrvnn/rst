9ba3e5d11a2a1dba1c483ec76912d8539e8d6b14
Saravanan Ganesh <srrvnn@live.com>
2016-12-14 22:23:51 -0800

    set max width for content at 1080px

--- public/js/components/Header.react.js
+++ public/js/components/Header.react.js
@@ -7,9 +7,11 @@ var User = require('./User.react');
 var Header = React.createClass({
 	render: function() {
 		return (
-			<div className="header">
-				<h1>R.S. Tuition Centre</h1>
-				<img src="/assets/lakshmirs.jpg" />
+			<div className="header-wrapper">
+				<div className="header">
+					<h1>R.S. Tuition Centre</h1>
+					<img src="/assets/lakshmirs.jpg" />
+				</div>
 			</div>
 		);
 	}
