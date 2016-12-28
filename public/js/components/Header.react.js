81ff24b435153e9beb54bec8c0c4d32b17f54b2a
Saravanan Ganesh <srrvnn@live.com>
2016-12-28 09:41:30 -0800

    add a transition on the header when away from home

--- public/js/components/Header.react.js
+++ public/js/components/Header.react.js
@@ -6,9 +6,14 @@ var User = require('./User.react');
 var Link = require('react-router').Link;
 
 var Header = React.createClass({
+
+	contextTypes: {
+		router: React.PropTypes.object
+	},
+
 	render: function() {
 		return (
-			<div className="header">
+			<div className={this.context.router.location.pathname == '/' ? "header" : "header minimal"}>
 				<div className="header-content">
 					<h1 className="title"><Link to="/">R.S. Tuition Centre</Link></h1>
 					<img className="user-pic" src="/assets/lakshmirs.jpg" />
